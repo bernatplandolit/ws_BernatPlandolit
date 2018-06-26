3c73b2205a8f2058c33a8fe3007369381129280f
bernatplandolit <40602121+bernatplandolit@users.noreply.github.com>
2018-06-26 17:16:38 +0200

    Added changed SW

--- BernatPlandolit_starwarscast.R
+++ BernatPlandolit_starwarscast.R
@@ -1,19 +1,23 @@
 ## ----eval=FALSE----------------------------------------------------------
 install.packages("rvest", dependencies=TRUE)
 require(rvest)
-lego_movie <- read_html("http://www.imdb.com/title/tt1490017/")
 
 # get urls
-#-------------
+list_movies <- read_html("https://www.imdb.com/list/ls070150896/")
+l_mv <- list_movies %>%
+  html_nodes("h3 a") %>%
+  html_attr("href")
 
-films <- c("https://www.imdb.com/title/tt2527336/?ref_=nv_sr_3",
-           "https://www.imdb.com/title/tt0120915/?ref_=nv_sr_7",
-           "https://www.imdb.com/title/tt0121766/?ref_=nv_sr_8",
-           "https://www.imdb.com/title/tt0076759/?ref_=nv_sr_5",
-           "https://www.imdb.com/title/tt0080684/?ref_=nv_sr_5",
-           "https://www.imdb.com/title/tt0086190/?ref_=nv_sr_7",
-           "https://www.imdb.com/title/tt0121765/?ref_=nv_sr_1",
-           "https://www.imdb.com/title/tt2488496/?ref_=ttls_li_tt")
+films <- paste0("https://www.imdb.com", l_mv[1:8])
+
+#films <- c("https://www.imdb.com/title/tt2527336/?ref_=nv_sr_3",
+ #          "https://www.imdb.com/title/tt0120915/?ref_=nv_sr_7",
+  #         "https://www.imdb.com/title/tt0121766/?ref_=nv_sr_8",
+   #        "https://www.imdb.com/title/tt0076759/?ref_=nv_sr_5",
+    #       "https://www.imdb.com/title/tt0080684/?ref_=nv_sr_5",
+     #      "https://www.imdb.com/title/tt0086190/?ref_=nv_sr_7",
+      #     "https://www.imdb.com/title/tt0121765/?ref_=nv_sr_1",
+       #    "https://www.imdb.com/title/tt2488496/?ref_=ttls_li_tt")
 
 cast <- list()
 for(i in seq_along(films)){
