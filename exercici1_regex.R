a7a41be787e7af72a559b361e454e45eb7243580
bernatplandolit <40602121+bernatplandolit@users.noreply.github.com>
2018-06-27 17:28:07 +0200

    Treball

--- exercici1_regex.R
+++ exercici1_regex.R
@@ -4,3 +4,10 @@
 text1 <- "The current year is 2017"
 my_pattern <- "[:digit:]"
 grepl(my_pattern, text1)
+
+
+# Exercice 11 -------------------------------------------------------------
+
+data <- read.csv2("LipidData.csv")
+
+carbons <- grep()
